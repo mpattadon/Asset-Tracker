11644e6f643e74f1ff852d50a20ca45addfca61a
Chattrakul Pattadon (2023) ZMAC679 <zmac679@live.rhul.ac.uk>
2025-11-21 02:28:29 +0000

    ini commit

---
+++ backend/src/test/java/com/assettracker/AssetTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assettracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
